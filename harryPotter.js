4edf0384efa7b1459074a3778914573e41543757
hlbattye <helen.battye@hotmail.com>
2021-02-27 17:42:18 +0000

    changed loop condition

--- harryPotter.js
+++ harryPotter.js
@@ -41,47 +41,28 @@ function clickOK() {
   do {
     let houseNumber = Math.floor(Math.random() * 4) + 1;
     console.log(houseNumber);
-    IsHouseFull()
-    addtohouse(houseNumber);
-    console.log(houseTotals);
-
-    if (houseNumber === 1) {
-      allocateHouse("Hufflepuff", hufflepuffs, "Hufflepuffs", nameInput);
-      // console.log("Hufflepuff");
-      // document.getElementById("result").innerHTML = nameInput + " you are in Hufflepuff house";
-      // hufflepuffs.push(nameInput);
-      // document.getElementById("Hufflepuffs").innerHTML = hufflepuffs.join('</p> <p>');
-      // resetName();
-    }
-    else if (houseNumber === 2) {
-      allocateHouse("Ravenclaw", ravenclaws, "Ravenclaws", nameInput);
-      // console.log("Ravenclaw");
-      // document.getElementById("result").innerHTML = nameInput + " you are in Ravenclaw house";
-      // ravenclaws.push(nameInput);
-      // document.getElementById("Ravenclaws").innerHTML = ravenclaws.join('</p> <p>');
-      // resetName();
-    }
-
-    else if (houseNumber === 3) {
-      console.log("Slytherin");
-      document.getElementById("result").innerHTML = nameInput + " you are in Slytherin house";
-      slytherins.push(nameInput);
-      document.getElementById("Slytherins").innerHTML = slytherins.join('</p> <p>');
-      resetName();
-    }
-    else {
-      console.log("Gryffindor");
-      document.getElementById("result").innerHTML = nameInput + " you are in Gryffindor house";
-      // gryffindors.push(nameInput);
-      // document.getElementById("Gryffindors").innerHTML = gryffindors.join('</p> <p>');
-      let x = document.createElement("TD");
-      let t = document.createTextNode(nameInput);
-      x.appendChild(t);
-      document.getElementById("Gryffindors").appendChild(x);
-      resetName();
-    }
-    break
   }
-  while (true);
+  while (IsHouseFull(housenumber))
+  addtohouse(houseNumber);
+  console.log(houseTotals);
+
+  if (houseNumber === 1) {
+    allocateHouse("Hufflepuff", hufflepuffs, "Hufflepuffs", nameInput);
+    // console.log("Hufflepuff");
+    // document.getElementById("result").innerHTML = nameInput + " you are in Hufflepuff house";
+    // hufflepuffs.push(nameInput);
+    // document.getElementById("Hufflepuffs").innerHTML = hufflepuffs.join('</p> <p>');
+    // resetName();
+  }
+  else if (houseNumber === 2) {
+    allocateHouse("Ravenclaw", ravenclaws, "Ravenclaws", nameInput);
+  }
+
+  else if (houseNumber === 3) {
+    allocateHouse("Slytherin", slytherins, "Slytherins", nameInput);
+  }
+  else {
+    allocateHouse("Gryffindor", gryffindors, "Gryffindors", nameInput);
+  }
 }
 
